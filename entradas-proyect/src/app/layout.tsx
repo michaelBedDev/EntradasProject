558936ac3700a845cf179753fb429be7a15f48f8
michaelBedDev@gmail.com <micama2000@gmail.com>
2025-05-09 13:42:12 +0200

    actualizados providers

--- entradas-proyect/src/app/layout.tsx
+++ entradas-proyect/src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import { AppSidebar } from "@/components/app";
 import { Navbar } from "@/components/app";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
-import { RainbowkitProviders } from "@/providers/rainbowkit/RainbowKitProvider";
+
+import { Web3AuthProvider } from "@/providers/Web3AuthProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import AuthSync from "@/providers/AuthSync";
-
-// import { getServerSession } from "next-auth";
 
 const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist" });
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
@@ -21,25 +21,26 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <RainbowkitProviders>
+        <Web3AuthProvider>
           {/* 👉 Aquí sincronizamos Supabase con la sesión de NextAuth */}
           <AuthSync />
+
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange>
             <SidebarProvider defaultOpen={defaultOpen}>
-              <div className="flex min-h-screen w-screen max-w-none overflow-x-hidden">
+              <div className="flex min-h-screen w-screen overflow-x-hidden">
                 <AppSidebar />
                 <div className="flex-1 flex flex-col">
                   <Navbar />
@@ -48,7 +49,7 @@ export default async function RootLayout({
               </div>
             </SidebarProvider>
           </ThemeProvider>
-        </RainbowkitProviders>
+        </Web3AuthProvider>
       </body>
     </html>
   );
